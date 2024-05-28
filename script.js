1952e6b40ac90fd2b446ef2df7f8172c5e6a2cda
JoyDhar32 <joyustcian@gmail.com>
2024-05-28 15:29:47 +1000

    improving performace

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ THREE.BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
 THREE.BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
 THREE.Mesh.prototype.raycast = acceleratedRaycast;
 
+
 const noise2D = createNoise2D();
 
 let camera, scene, renderer, controls;
@@ -29,35 +30,26 @@ const direction = new THREE.Vector3();
 const vertex = new THREE.Vector3();
 const color = new THREE.Color();
 
+
+//addingCode
 const FLOOR_SIZE = 2000;
 let generateFloor;
 const floors = {};
 
-const boxGeometry = new THREE.BoxGeometry(20, 20, 20).toNonIndexed();
-const boxMaterial = new THREE.MeshPhongMaterial({ specular: 'purple', flatShading: true, vertexColors: true });
-boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
-const sphereGeometry = new THREE.SphereGeometry(5, 48, 48); // Adjust radius, segments for smoothness
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: '#9D941B' }); // Set a single color
-const cylinderGeometry = new THREE.CylinderGeometry(8, 8, 8, 64); // Adjust radius top/bottom, height, segments for smoothness
-const cylinderMaterial = new THREE.MeshBasicMaterial({ color: 'orange' });
-boxGeometry.computeBoundsTree();
-sphereGeometry.computeBoundsTree();
-cylinderGeometry.computeBoundsTree();
 
 init();
 animate();
 
 function init() {
-    THREE.ColorManagement.enabled = false;
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.y = 10;
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color('#87CEEB');
-    scene.fog = new THREE.Fog('#87CEEB', 0, 750);
+    scene.background = new THREE.Color(0xffffff);
+    scene.fog = new THREE.Fog(0xffffff, 0, 750);
 
-    const light = new THREE.HemisphereLight(0xeeeeff, 'blue', 2.5);
+    const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 2.5);
     light.position.set(0.5, 1, 0.75);
     scene.add(light);
 
@@ -154,130 +146,78 @@ function init() {
 
     raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10);
 
-    const floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: true });
-
-    generateFloor = (x, z) => {
-        console.log('generating floor', x, z);
-        
-        // geometry
-        let floorGeometry = new THREE.BufferGeometry();
-        const subdivisions = 100;
-        const indices = [];
-        const vertices = [];
-        for (let i = 0; i <= subdivisions; i++) {
-            for (let j = 0; j <= subdivisions; j++) {
-                const x1 = i / subdivisions;
-                const z1 = j / subdivisions;
-                vertices.push(x1 * FLOOR_SIZE - FLOOR_SIZE / 2, noise2D(x + x1, z + z1) * FLOOR_SIZE / 10, z1 * FLOOR_SIZE - FLOOR_SIZE / 2);
-            }
-        }
-        for (let x = 0; x < subdivisions; x++) {
-            for (let z = 0; z < subdivisions; z++) {
-                const a = z + x * (subdivisions + 1);
-                const b = (z + 1) + x * (subdivisions + 1);
-                const c = z + (x + 1) * (subdivisions  + 1);
-                const d = (z + 1) + (x + 1) * (subdivisions  + 1);
-                indices.push(a, b, d);
-                indices.push(d, c, a);
-            }
-        }
-        floorGeometry.setIndex(indices);
-        floorGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-        floorGeometry.computeVertexNormals();
-
-        
-        // vertex displacement
-
-        let position = floorGeometry.attributes.position;
-
-        for (let i = 0, l = position.count; i < l; i++) {
-
-            vertex.fromBufferAttribute(position, i);
-
-            const isEdge = vertex.x === FLOOR_SIZE / 2 || vertex.x === -FLOOR_SIZE / 2 || vertex.z === FLOOR_SIZE / 2 || vertex.z === -FLOOR_SIZE / 2;
-            if (!isEdge) {
-                vertex.x += Math.random() * FLOOR_SIZE / 100 - FLOOR_SIZE / 200;
-                vertex.y += Math.random() * FLOOR_SIZE / 1000;
-                vertex.z += Math.random() * FLOOR_SIZE / 100 - FLOOR_SIZE / 200;
-                vertex.x = Math.min(FLOOR_SIZE / 2, Math.max(- FLOOR_SIZE / 2, vertex.x));
-                vertex.z = Math.min(FLOOR_SIZE / 2, Math.max(- FLOOR_SIZE / 2, vertex.z));
-            }
-
-            position.setXYZ(i, vertex.x, vertex.y, vertex.z);
-
-        }
-
-        floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
-
-        position = floorGeometry.attributes.position;
-        const colorsFloor = [];
-
-        for (let i = 0, l = position.count; i < l; i++) {
-
-            color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
-            colorsFloor.push(color.r, color.g, color.b);
-
-        }
-
-        floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
+    // floor
 
 
-        const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-        position = boxGeometry.attributes.position;
-        const colorsBox = [];
+    
+    let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
+    floorGeometry.rotateX(- Math.PI / 2);
 
-        for (let i = 0, l = position.count; i < l; i++) {
+    // vertex displacement
 
-            color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
-            colorsBox.push(color.r, color.g, color.b);
+    let position = floorGeometry.attributes.position;
 
-        }
+    for (let i = 0, l = position.count; i < l; i++) {
 
-        boxGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
+        vertex.fromBufferAttribute(position, i);
 
-        // Creating Boxes
+        vertex.x += Math.random() * 20 - 10;
+        vertex.y += Math.random() * 2;
+        vertex.z += Math.random() * 20 - 10;
 
-        floorGeometry.computeBoundsTree();
-        for (let i = 0; i < FLOOR_SIZE / 100; i++) {
-            let range = FLOOR_SIZE / 2;
-            const box = new THREE.Mesh(boxGeometry, boxMaterial);
-            const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-            const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+        position.setXYZ(i, vertex.x, vertex.y, vertex.z);
 
-            const setY = (item) => {
-                const raycaster = new THREE.Raycaster(new THREE.Vector3(item.position.x, 1000, item.position.z), new THREE.Vector3(0, -1, 0));
-                raycaster.firstHitOnly = true;
-                const intersects = raycaster.intersectObject(floor);
-                if (intersects.length > 0) item.position.y = intersects[0].point.y + 10;
-            }
-
-            box.position.x = Math.random() * range * 2 - range; // Ensures values between -floorSize and floorSize
-            box.position.z = Math.random() * range * 2 - range; // Ensures values between -floorSize and floorSize
-            sphere.position.x = Math.random() * range * 2 - range; // Ensures values between -floorSize and floorSize
-            sphere.position.z = Math.random() * range * 2 - range; // Ensures values between -floorSize and floorSize
-            cylinder.position.x = Math.random() * range * 2 - range; // Ensures values between -floorSize and floorSize
-            cylinder.position.z = Math.random() * range * 2 - range; // Ensures values between -floorSize and floorSize
-            setY(box);
-            setY(sphere);
-            setY(cylinder);
-
-            floor.add(box);
-            floor.add(sphere);
-            floor.add(cylinder);
-            objects.push(box);
-            objects.push(sphere);
-            objects.push(cylinder);
-
-        }
-
-        floor.position.x = x * FLOOR_SIZE;
-        floor.position.z = z * FLOOR_SIZE;
-        floors[`${x},${z}`] = floor;
     }
 
-    generateFloor(0, 0);
+    floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
 
+    position = floorGeometry.attributes.position;
+    const colorsFloor = [];
 
+    for (let i = 0, l = position.count; i < l; i++) {
+
+        color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
+        colorsFloor.push(color.r, color.g, color.b);
+
+    }
+
+    floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
+
+    const floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: true });
+
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    scene.add(floor);
+
+    // objects
+
+    const boxGeometry = new THREE.BoxGeometry(10, 10, 10).toNonIndexed();
+
+    position = boxGeometry.attributes.position;
+    const colorsBox = [];
+
+    for (let i = 0, l = position.count; i < l; i++) {
+
+        color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
+        colorsBox.push(color.r, color.g, color.b);
+
+    }
+
+    boxGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
+
+    for (let i = 0; i < 500; i++) {
+
+        const boxMaterial = new THREE.MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
+        boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75, THREE.SRGBColorSpace);
+
+        const box = new THREE.Mesh(boxGeometry, boxMaterial);
+        box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
+        box.position.y = Math.floor(Math.random() * 1) * 10 + 10;
+        box.position.z = Math.floor(Math.random() * 200 - 10) * 20;
+
+        scene.add(box);
+        objects.push(box);
+
+    }
 
     //
 
@@ -306,13 +246,14 @@ function animate() {
     requestAnimationFrame(animate);
 
     const time = performance.now();
-
+let playerHeight = 10;
+let groundHeight = 10;
     if (controls.isLocked === true) {
 
         raycaster.ray.origin.copy(controls.getObject().position);
-        raycaster.ray.origin.y -= 10;
+        raycaster.ray.origin.y += playerHeight;
 
-        const intersections = raycaster.intersectObjects(objects, false);
+        const intersections = raycaster.intersectObjects(objects, true);
 
         const onObject = intersections.length > 0;
 
@@ -332,33 +273,24 @@ function animate() {
 
         if (onObject === true) {
 
-            velocity.y = Math.max(0, velocity.y);
             canJump = true;
-
+            console.log("onObject")
+            if (moveForward) {
+                moveForward = false; // Block forward movement
+                velocity.z = 0; // Reset forward velocity to zero
+            }
+       
         }
 
-        controls.moveRight(- velocity.x * delta * 10);
-        controls.moveForward(- velocity.z * delta * 10);
+        controls.moveRight(- velocity.x * delta);
+        controls.moveForward(- velocity.z * delta);
 
         controls.getObject().position.y += (velocity.y * delta); // new behavior
 
-        const controlsPos = controls.getObject().position;
-        const [x, z] = [Math.round(controlsPos.x / FLOOR_SIZE), Math.round(controlsPos.z / FLOOR_SIZE)];
-        if (!floors[`${x},${z}`]) generateFloor(x, z);
-        if (!floors[`${x + 1},${z}`]) generateFloor(x + 1, z);
-        if (!floors[`${x - 1},${z}`]) generateFloor(x - 1, z);
-        if (!floors[`${x},${z + 1}`]) generateFloor(x, z + 1);
-        if (!floors[`${x},${z - 1}`]) generateFloor(x, z - 1);
-        if (!floors[`${x + 1},${z + 1}`]) generateFloor(x + 1, z + 1);
-        if (!floors[`${x - 1},${z + 1}`]) generateFloor(x - 1, z + 1);
-        if (!floors[`${x + 1},${z - 1}`]) generateFloor(x + 1, z - 1);
-        if (!floors[`${x - 1},${z - 1}`]) generateFloor(x - 1, z - 1);
-
-
-        if (controls.getObject().position.y < 10) {
+        if (controls.getObject().position.y <groundHeight) {
 
             velocity.y = 0;
-            controls.getObject().position.y = 10;
+            controls.getObject().position.y = groundHeight;
 
             canJump = true;
 
@@ -369,5 +301,5 @@ function animate() {
     prevTime = time;
 
     renderer.render(scene, camera);
-
 }
+
